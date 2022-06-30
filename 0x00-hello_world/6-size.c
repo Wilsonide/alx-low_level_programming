22ed40263e4cba9ce4d43bd97832543ace60f57f
Wilson Icheku <ichekuwilson538@gmail.com>
2022-06-30 16:02:10 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,17 @@
  */
 int main(void)
 {
+char A;
+int B;
+long int C;
+long long int D;
+float f;
 
-printf("size of char is : %d byte\n", sizeof(char));
-printf("size of int is : %d byte\n", sizeof(int));
-printf("size of long int is : %d byte\n", sizeof(long int));
-printf("size of long long int is : %d byte\n", sizeof(long long int))
-printf("size of float is : %d byte\n", sizeof(float));
+printf("size of char is : %d byte\n", sizeof(A));
+printf("size of int is : %d byte\n", sizeof(B));
+printf("size of long int is : %d byte\n", sizeof(C));
+printf("size of long long int is : %d byte\n", sizeof(D))
+printf("size of float is : %d byte\n", sizeof(f));
 
 return (0);
 
