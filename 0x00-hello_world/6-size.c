1ed99c4be2e4a661ab6f3354e4c28199dab633ee
Wilson Icheku <ichekuwilson538@gmail.com>
2022-06-30 15:52:10 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,9 @@ int main(void)
 
 printf("size of char is : %d byte\n", sizeof(char));
 printf("size of int is : %d byte\n", sizeof(int));
-printf("size of short int is : %d byte\n", sizeof(short int));
-
 printf("size of long int is : %d byte\n", sizeof(long int));
-
+printf("size of long long int is : %d byte\n", sizeof(long long int))
 printf("size of float is : %d byte\n", sizeof(float));
-printf("size of double is : %d byte\n", sizeof(double));
-printf("size of long double is : %d byte\n", sizeof(long double));
 
 return (0);
 
