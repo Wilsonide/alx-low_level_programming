bc160bc5fa6ea24634785f9e1a75fd0f32741e0b
Wilson Icheku <ichekuwilson538@gmail.com>
2022-06-30 14:03:31 -0700

    First c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - code executes from here
- * Return: does not return anything
+ * main - prints a string
+ * Return: returns nothing
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 
 }
