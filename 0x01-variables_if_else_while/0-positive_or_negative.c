7be1bfae853c79dcaaef369f283eae82d770933b
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-01 07:31:00 -0700

    first if/else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - if/else block
+ * Return: return success
+ */
+int main(void)
+{
+if (n > 0)
+	{
+	printf(n, "is positive");
+	}
+
+else if (n < 0)
+	{
+	printf(n, "is negative");
+	}
+
+else if (n == 0)
+	{
+	printf(n, "is zero");
+	}
+}
+return (0);
+
+
