3dcc096642aa4fe6e3bc0c20cae1a7c8eda78d94
Wilson Icheku <ichekuwilson538@gmail.com>
2022-06-30 22:25:14 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,10 @@
  * Return: Error
  */
 int main(void)
-stderr = "Anything"
 {
 
 fprintf(stderr, "and that piece of art is useful\"");
 fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
-return (1);
+return ("Anything");
 
 }
