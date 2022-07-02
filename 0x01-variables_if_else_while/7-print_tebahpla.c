8bc9b0ed771b78343cc433bba2dae90d22b07034
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-01 20:22:20 -0700

    reversed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Reversed alphabet
+ * Return: All success
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch++)
+	{
+	putchar(ch);
+	}
+putchar('\n');
+return (0);
+}
