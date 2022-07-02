8e19601babbc90816a9686e5f72a72c0b560cffd
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-01 19:50:57 -0700

    Unformatted output

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - unformatted output display function
+ * Return: Success
+ */
+int main(void)
+{
+int vi
+for (vi = 0; vi < 10; vi++)
+	{
+	putchar(vi);
+	}
+putchar('\n');
+return (0);
+}
