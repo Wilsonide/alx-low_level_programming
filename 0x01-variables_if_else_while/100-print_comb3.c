e3878a0afdd2155fd64c2d990f6b4bb692fc7338
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-01 22:20:20 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ for (num1 = 0; num1 <= 98; num1++)
 		{
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
-		if (num2 == num1 +1 && num2 == 99)
+		if (num2 == num1 + 1 && num2 == 99)
 			continue;
-		putchar(',');
-		putchar(' ');
+	putchar(',');
+	putchar(' ');
 		}
 putchar('\n');
 return (0);
