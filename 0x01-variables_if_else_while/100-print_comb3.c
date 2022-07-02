9decd3f563150a8a0024fcfaac32e404a6febfd5
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-01 21:57:05 -0700

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Combines two digit
+ * Return: Success
+ */
+int main(void)
+{
+int num;
+int ch;
+for (num = 0; num <= 98; num++)
+	for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+		putchar((num1 / 10) + '0');
+		putchar((num1 % 10) + '0');
+		putchar(' ');
+		putchar((num2 / 10) + '0');
+		putchar((num2 % 10) + '0');
+		if (num1 == 98 && num2 == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+		}
+putchar('\n');
+return (0);
+}
