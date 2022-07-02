4a393810fd8648ca9362cf26789fe5fdd1391305
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-01 22:01:38 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-int num;
+int num1;
 int num2;
-for (num = 0; num <= 98; num++)
+for (num1 = 0; num1 <= 98; num++)
 	for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 		putchar((num1 / 10) + '0');
