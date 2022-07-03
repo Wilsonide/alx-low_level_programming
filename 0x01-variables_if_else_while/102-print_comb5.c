a523b9ce09c466bb8ccc2c99d023375974c56747
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-03 16:41:15 -0700

    Advanced task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,8 @@ for (num1 = 0; num1 <= 98; num1++)
 		putchar((num1 / 10) + '0');
 		putchar((num1 % 10) + '0');
 		putchar(' ');
-		putchar((num2 % 10) + '0');
 		putchar((num2 / 10) + '0');
+		putchar((num2 % 10) + '0');
 		if (num1 == 98 && num2 == 99)
 			continue;
 		putchar(',');
