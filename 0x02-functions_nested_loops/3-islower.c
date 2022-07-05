5d300e46681128852195714f447c10161f2d0af1
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-05 12:03:36 -0700

    islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Function declaration
+ * Return: returns an int
+ * @c: single number input
+ */
+int _islower(int c)
+{
+if (c == 'a' && c >= 'z')
+	{
+	return (1);
+else
+	{
+	return (0);
+}
