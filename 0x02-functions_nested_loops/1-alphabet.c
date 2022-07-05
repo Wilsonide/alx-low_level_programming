f6d5511ea0a7fff21746f16fff191b318f1a7c88
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-05 10:49:50 -0700

    lower case alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#define "main.h"
+/**
+ * print_alphabet - function that prints alphabets in lower case
+ */
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	_putchar(alpha);
+	}
+_putchar('\n');
+}
