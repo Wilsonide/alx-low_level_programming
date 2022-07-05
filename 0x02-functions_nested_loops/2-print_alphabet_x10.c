c27601ddb37d99edc7bc7029fec2f16f5480a0b6
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-05 11:37:03 -0700

    lower case alphabet x10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@ void print_alphabet_x10(void)
 {
 int n;
 char lr
-while (n <= 10)
+while (n = 9)
 	{
 	for (lr = 'a'; lr <= 'z'; lr++)
 		{
 		_putchar(lr);
 		}
 	n++;
+	_putchar('\n');
 	}
-_putchar('\n');
 }
