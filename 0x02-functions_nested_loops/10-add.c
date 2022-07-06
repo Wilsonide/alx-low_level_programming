16dca0f27a1ca006ad401399a3da61f76d767700
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-05 18:50:22 -0700

    function10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sums two numbers
+ * Return: returns the sum as int
+ * @one: first input
+ * @two: second input
+ */
+int add(int one, int two)
+{
+return (one + two);
+}
