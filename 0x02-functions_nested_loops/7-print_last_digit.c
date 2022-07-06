e585366d9c8435f95119296d414ca23041b3114c
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-05 17:56:23 -0700

    function7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - Outputs the last digit of a numbet
+ * Return: returns an int
+ * @r: int input
+ */
+int print_last_digit(int r)
+{
+return ((r % 10) + '0');
+}
