08692219f7c6819546e55837745ac5d0b818aa30
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-06 21:17:51 -0700

    advanced

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: success
+ */
+int main(void)
+{
+int sum;
+int i;
+for (i = 0; i <= 1024; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+		{
+		sum = sum + i;
+		printf("%d", sum);
+		}
+	}
+return (0);
+}
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,11 +6,18 @@
 int main(void)
 {
 int num1 = 1;
+int sum = 0;
 int num2 = 2;
 int i;
 for (i = 3; i <= 10; i++)
 	{
 	num3 = num1 + num2;
 	printf("%d", num3);
+	sum = num1 + num2 + num3;
 	num1 = num2;
-	num2 = i;
+	num2 = num3;
+	if (sum <= 4000000)
+		{
+		printf("%d", num1);
+		printf("%d", num1);
+		printf("%d", num1);
