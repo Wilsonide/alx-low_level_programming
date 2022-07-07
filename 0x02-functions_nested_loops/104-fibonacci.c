a4609df0e892919d07cc1478da37c6b357e28f42
Wilson Icheku <ichekuwilson538@gmail.com>
2022-07-06 20:10:45 -0700

    advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,11 +10,17 @@ int a = 1;
 int b = 2;
 int c;
 printf("%d", a);
+putchar(',');
+putchar(' ');
 printf("%d", b);
+putchar(',');
+putchar(' ');
 for (i = 3; i <= 98; i++)
 	{
 	c = a + b;
 	printf("%d", c);
+	putchar(',');
+	putchar(' ');
 	a = b;
 	b = c;
 	}
