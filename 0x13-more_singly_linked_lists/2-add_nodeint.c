79c1407668c15bda78a8d7e0910fcf673f26747a
Wilson Icheku <ichekuwilson538@gmail.com>
2022-08-16 16:03:09 -0700

    up and running

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,11 +9,12 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+listint_t *new;
 listint_t *new = malloc(size of(listint_t));
 if (new == NULL)
 	return (NULL);
 new->n = n;
-new->next = *head;
-*head = new;
+new->next = head;
+head = new;
 return (new);
 }
