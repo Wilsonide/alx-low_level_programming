e140e09d7c01771621c168e92c7b5afe7437f0e8
Wilson Icheku <ichekuwilson538@gmail.com>
2022-08-16 12:10:33 -0700

    up and running

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "list.h"
+#include <stdlib.h>
+/*
+ * print_listint - name of function
+ * @h: pointer to the list elements
+ * Return:number of element in list
+ */
+size_t print_listint(const listint_t *h)
+{
+h = malloc(sizeof(listint));
+listint_t *temp = h;
+size_t count = 0;
+if (temp == NULL)
+	printf("list is empty");
+while (temp != NULL)
+	{
+	printf("%d\n", temp->n);
+	temp = temp->next;
+	count++;
+	}
+return (count);
+}
